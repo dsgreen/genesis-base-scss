1998c30e1963b86b10c813645e9ec7238a61d083
Doug Green <doug@dsgreen.com>
2015-12-15 19:31:50 -0500

    Update theme name in functions file

--- functions.php
+++ functions.php
@@ -3,9 +3,9 @@
 include_once( get_template_directory() . '/lib/init.php' );
 
 //* Child theme (do not remove)
-define( 'CHILD_THEME_NAME', 'Genesis Base Theme' );
-define( 'CHILD_THEME_URL', 'http://www.douglasgreen.com' );
-define( 'CHILD_THEME_VERSION', '1.0' );
+define( 'CHILD_THEME_NAME', 'Genesis Sample Theme' );
+define( 'CHILD_THEME_URL', 'http://www.studiopress.com/' );
+define( 'CHILD_THEME_VERSION', '2.2.2' );
 
 //* Enqueue Google Fonts
 add_action( 'wp_enqueue_scripts', 'genesis_sample_google_fonts' );
